9ee587c21bc087571ab6678600bd6859b4814b89
Lucas Seixas <seixas.lc@gmail.com>
2016-10-18 21:01:59 -0200

    LM01 Initial Commit .gitgnore for Unreal

---
+++ BuildingScape/Source/BuildingScape/BuildingScapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingScapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGSCAPE_API ABuildingScapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
